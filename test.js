b20e65eda4f2af62c60b6cd67c9ab1b32a4510b1
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-02 01:51:51 +0700

    tweaks

--- test.js
+++ test.js
@@ -1,39 +1,34 @@
-'use strict';
-var test = require('ava');
-var binVersion = require('./');
+import test from 'ava';
+import fn from './';
 
-test('does-not-exist',function(t) {
-	t.plan(2);
-
-	binVersion('does-not-exist', function (err) {
+test.cb('does-not-exist', function (t) {
+	fn('does-not-exist', function (err) {
 		t.assert(err.code, 'ENOENT');
 		t.assert(err.message, 'Couldn\'t find the `does-not-exist` binary. Make sure it\'s installed in your $path');
+		t.end();
 	});
 });
 
-test('curl', function (t) {
-	t.plan(2);
-
-	binVersion('curl', function (err, version) {
-		t.assert(!err, err);
-		t.assert(/\d+\.\d+\.\d+/.test(version), version);
+test.cb('curl', function (t) {
+	fn('curl', function (err, version) {
+		t.ifError(err);
+		t.true(/\d+\.\d+\.\d+/.test(version), version);
+		t.end();
 	});
 });
 
-test('npm', function (t) {
-	t.plan(2);
-
-	binVersion('npm', function (err, version) {
-		t.assert(!err, err);
-		t.assert(/\d+\.\d+\.\d+/.test(version), version);
+test.cb('npm', t => {
+	fn('npm', (err, version) => {
+		t.ifError(err);
+		t.true(/\d+\.\d+\.\d+/.test(version), version);
+		t.end();
 	});
 });
 
-test('openssl', function (t) {
-	t.plan(2);
-
-	binVersion('openssl', {args: ['version']}, function (err, version) {
+test.cb('openssl', function (t) {
+	fn('openssl', {args: ['version']}, function (err, version) {
 		t.assert(!err, err);
 		t.assert(/\d+\.\d+\.\d+/.test(version), version);
+		t.end();
 	});
 });
