c8803f9185ab22723e388b5d71cfa3aa553b8f6d
Kevin Martensson <kevinmartensson@gmail.com>
2019-01-22 14:56:34 +0100

    Add support for using multiple commands
    Fixes #12.

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 'use strict';
 const execa = require('execa');
 const findVersions = require('find-versions');
+const pLocate = require('p-locate');
+
+const subCommandBinaries = [
+	'openssl',
+	'swiftlint'
+];
+
+const getArgs = (binary, args) => {
+	if (Array.isArray(args)) {
+		return [args];
+	}
+
+	if (subCommandBinaries.includes(binary)) {
+		return [['version']];
+	}
+
+	return [
+		['--version'],
+		['version']
+	];
+};
+
+const getVersion = (binary, args) => execa(binary, args)
+	.then(result => findVersions(result.stdout || result.stderr, {loose: true})[0])
+	.catch(error => {
+		if (error.code === 'ENOENT') {
+			error.message = `Couldn't find the \`${binary}\` binary. Make sure it's installed and in your $PATH`;
+		}
+
+		throw error;
+	});
 
 module.exports = (binary, options = {}) => {
-	return execa(binary, options.args || ['--version'])
-		.then(result => findVersions(result.stdout || result.stderr, {loose: true})[0])
-		.catch(error => {
-			if (error.code === 'ENOENT') {
-				error.message = `Couldn't find the \`${binary}\` binary. Make sure it's installed and in your $PATH`;
-			}
-
-			throw error;
-		});
+	const versions = getArgs(binary, options.args).map(argument => getVersion(binary, argument));
+	return pLocate(versions, version => Boolean(version), {concurrency: 1});
 };
