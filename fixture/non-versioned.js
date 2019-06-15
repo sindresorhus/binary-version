c36f719a23cae58635f2ddf9837a0738742fc8fe
Yanis Benson <yanis.benson@gmail.com>
2019-06-15 10:01:13 +0300

    fixtures

---
+++ fixture/non-versioned.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+'use strict';
+if (process.argv[2] === '--version') {
+	process.exit(1);
+}
---
+++ fixture/versioned-type1.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+'use strict';
+if (process.argv[2] === '--version') {
+	console.log('1.2.3');
+}
